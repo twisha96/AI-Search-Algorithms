134752c8c5eda9174ebc4b8f254de99a5b6f0fd6
Twisha Naik <twishanaik@nbp-227-71.nbp.ruw.rutgers.edu>
2019-09-18 16:13:56 -0400

    made changes to the function definations

--- a_star.py
+++ a_star.py
@@ -3,36 +3,35 @@ from Queue import PriorityQueue
 def manhattan_heuristic(x, y, dim):
 	return abs(x - dim)+abs(y - dim)
 
+def euclidian_heuristic(x, y, dim):
+	return (x - dim)^2+abs(y - dim)^2
+
 def source_dist(x, y):
 	return x+y
 
-def get_path():
+def get_path(x, y, maze):
 	path = []
-	(x,y)=(dim-1,dim-1)
 	while((x,y)!=(0,0)):
-    	print((x,y))
-    	path.insert(0,(x,y))
-    	(x, y) = maze[x][y].parent
-    path.insert(0,(0,0))
-    return path
+		print((x,y))
+		path.insert(0,(x,y))
+		(x, y) = maze[x][y].parent
+	path.insert(0,(0,0))
+	return path
 
-def get_neighbors(x, y, closed_set, maze, fringe):
+def get_neighbors(maze, x, y, dim, closed_set, fringe):
 	print(str(x)+" "+str(y))
 	print("In neighbor function: "+str(fringe.qsize()))
 	source_distance = source_dist(x,y) 
 
 	if(x-1>=0):
 		if(maze[x-1][y].value!=1 and (x-1,y) not in closed_set):
-			# neighbors.add[(x-1, y)]
 			maze[x-1][y].parent = (x,y)
 			priority_value = (source_distance + 1) + manhattan_heuristic(x-1, y, dim)
 			fringe.put((priority_value, (x-1, y)))
 			print("Left: "+str(fringe.qsize()))
 			
 	if(x+1<=dim-1):
-		# print(maze[x+1][y].value==0 and (x+1,y) not in closed_set)
 		if(maze[x+1][y].value!=1 and (x+1,y) not in closed_set):
-			# neighbors.add[(x+1, y)]
 			maze[x+1][y].parent = (x,y)
 			priority_value = (source_distance + 1) + manhattan_heuristic(x+1, y, dim)
 			fringe.put((priority_value, (x+1, y)))
@@ -40,16 +39,13 @@ def get_neighbors(x, y, closed_set, maze, fringe):
 	
 	if(y-1>=0):
 		if(maze[x][y-1].value!=1 and (x,y-1) not in closed_set):
-			# neighbors.add[(x, y-1)]
 			maze[x][y-1].parent = (x,y)
 			priority_value = (source_distance + 1) + manhattan_heuristic(x, y-1, dim)
 			fringe.put((priority_value, (x, y-1)))
 			print("Bottom: "+str(fringe.qsize()))
 	
 	if(y+1<=dim-1):
-		# print(maze[x][y+1].value==0 and (x,y+1) not in closed_set)
 		if(maze[x][y+1].value!=1 and (x,y+1) not in closed_set):
-			# neighbors.add[(x, y+1)]
 			maze[x][y+1].parent = (x,y)
 			priority_value = (source_distance + 1) + manhattan_heuristic(x, y+1, dim)
 			fringe.put((priority_value, (x, y+1)))
@@ -58,7 +54,6 @@ def get_neighbors(x, y, closed_set, maze, fringe):
 	return fringe
 
 def a_star_traversal(maze):
-
 	closed_set = set()
 	fringe = PriorityQueue()
 	fringe.put((0, (0, 0)))
@@ -71,7 +66,7 @@ def a_star_traversal(maze):
 		if((x,y)==(dim-1, dim-1)):
 			print "DONE"
 			return True
-		fringe = get_neighbors(x, y, closed_set, maze, fringe)
+		fringe = get_neighbors(maze, x, y, dim, closed_set, fringe)
 		closed_set.add((x,y))
 		print("Fringe: "+str(not(fringe.empty())))
 		# print(closed_set)
@@ -83,6 +78,13 @@ def a_star_traversal(maze):
 
 result = a_star_traversal(maze)
 if result:
-	path = get_path()
+	path = get_path(dim-1, dim-1, maze)
 	print(path)
 	print(len(path))
+
+
+"""
+Path for the intersting maze:
+[(0, 0), (0, 1), (0, 2), (1, 2), (2, 2), (3, 2), (4, 2), (5, 2), (6, 2), (6, 3), (6, 4), (5, 4), (5, 5), (4, 5), (4, 6), (4, 7), (5, 7), (6, 7), (6, 8), (7, 8), (7, 9), (8, 9), (9, 9)]
+23
+"""
