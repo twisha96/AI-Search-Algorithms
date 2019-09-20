c14774f92cf0c9df9ca4eff0ba5cc33d435a6358
Aditya Lakra <adityalakra@Adityas-MBP.home>
2019-09-20 02:14:08 -0400

    Add analysis to find dim and P0

--- bfs_2.py
+++ bfs_2.py
@@ -1,16 +1,3 @@
-"""
-checks for valid neighbours which are not present and in
-the closed set and adds them to the fringe. Following is the 
-order in which the neighbors are added to the fringe:
-	1) up
-	2) left
-	3) down
-	4) right
-Since the fringe is implemented using stack data structure,
-the order in which these neighbors are popped out is opposite
-thus making sure that we always try to go towards bottom right
-side of the fringe, ie. towards the goal.
-"""
 def get_neighbors(maze, x, y, dim, closed_set, fringe):
 	if(x-1>=0):
 		if(maze[x-1][y].value!=1 and (x-1,y) not in closed_set):
@@ -30,18 +17,17 @@ def get_neighbors(maze, x, y, dim, closed_set, fringe):
 			fringe.append((x, y+1))	
 	return fringe
 
-# traverses the graph using DFS algorithm
-def dfs_traversal(maze, dim):
+def bfs_traversal(maze, dim):
 	closed_set = set()
 	fringe = [(0,0)]
 	while(len(fringe) > 0):
-		((x,y)) = fringe.pop()
+		((x,y)) = fringe.pop(0)
 		if((x,y) in closed_set):
 			continue
 		if((x,y)==(dim-1, dim-1)):
-			print "Solution found"
+			#print "Solution found"
 			return True
 		fringe = get_neighbors(maze, x, y, dim, closed_set, fringe)
 		closed_set.add((x,y))
-	print "No Solution"
+	#print "No Solution"
 	return False
