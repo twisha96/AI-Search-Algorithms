6d404a0e7ee63bd6aeec91a0e43d18d4c6e8eb13
haripriya2703 <haripriyaponnekanti@gmail.com>
2019-09-20 21:05:40 -0400

    Modified according to the visited property of cell

--- CheckNeighbours.py
+++ CheckNeighbours.py
@@ -1,14 +1,18 @@
 class CheckNeighours():
     def check_neighbours(self, maze, dim, m, n, fringe, closed):
-        if m - 1 >= 0 and maze[m-1][n].value != 1 and (m - 1, n) not in closed:
+        if m - 1 >= 0 and maze[m-1][n].value != 1 and not maze[m-1][n].visited and (m - 1, n) not in closed:
             fringe.append((m - 1, n))
+            maze[m-1][n].visited = True
             maze[m - 1][n].parent = (m, n)
-        if n + 1 < dim and maze[m][n+1].value != 1 and (m, n + 1) not in closed:
+        if n + 1 < dim and maze[m][n+1].value != 1 and not maze[m][n+1].visited and (m, n + 1) not in closed:
             fringe.append((m, n + 1))
+            maze[m][n+1].visited = True
             maze[m][n + 1].parent = (m, n)
-        if m + 1 < dim and maze[m+1][n].value != 1 and (m + 1, n) not in closed:
+        if m + 1 < dim and maze[m+1][n].value != 1 and not maze[m+1][n].visited and (m + 1, n) not in closed:
             fringe.append((m + 1, n))
+            maze[m+1][n].visited = True
             maze[m + 1][n].parent = (m, n)
-        if n - 1 >= 0 and maze[m][n-1].value != 1 and (m, n - 1) not in closed:
+        if n - 1 >= 0 and maze[m][n-1].value != 1 and not maze[m][n-1].visited and (m, n - 1) not in closed:
             fringe.append((m, n - 1))
+            maze[m][n-1].visited = True
             maze[m][n - 1].parent = (m, n)
