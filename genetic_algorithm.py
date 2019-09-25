76de38248d72c0df200d94b28dbb27656a117410
Twisha Naik <twishanaik@Twishas-MacBook-Pro.local>
2019-09-25 01:05:24 -0400

    changed the parameters in the a_star function call

--- genetic_algorithm.py
+++ genetic_algorithm.py
@@ -1,4 +1,4 @@
-import maze_runner as mr
+import maze_runner
 import copy
 import random
 from Queue import PriorityQueue
@@ -6,52 +6,69 @@ import numpy as np
 from a_star import a_star_traversal
 import pdb
 
+def test_population(new_maze_population):
+	print "--------------------"
+	print "Population Solvability Check"
+	for curr_maze_count in range(new_maze_population.qsize()):
+		curr_maze = new_maze_population.queue[curr_maze_count]
+		curr_maze_result_dict = \
+			a_star_traversal(copy.deepcopy(curr_maze[1]), "manhattan")
+		if not curr_maze_result_dict["is_solvable"]:
+			print "ERROR!!!"
+			maze_runner.visualize_maze(copy.deepcopy(curr_maze[1]))
+		print curr_maze_result_dict["is_solvable"]
+	print "--------------------"
+
 # Main code
-dim = 10
-p = 0.4
+dim = 15
+p = 0.3
 
 # priority queue with fitness (i.e. hardness) associated with each maze
 maze_population = PriorityQueue()
-population_size = 20
-total_crossovers = 20
+population_size = 50
+total_crossovers = 50
 mutation_rate = 0
-generations = 100
+generations = 10
 
 for i in range(population_size):
 	while True:
-		maze = mr.get_maze(dim, p)
+		maze = maze_runner.get_maze(dim, p)
 		
 		# find path using a_star
-		manhattan_result, manhattan_steps, manhattan_max_fringe_length, manhattan_avg_fringe_length, \
-		manhattan_closed_set = a_star_traversal(copy.deepcopy(maze), "manhattan")
+		result_dict = a_star_traversal(copy.deepcopy(maze), "manhattan")
+		
 		# calculate fitness using the max number of nodes explored
-		maze_fitness = -manhattan_steps
-		if manhattan_result:
+		maze_fitness = -result_dict["total_steps"]
+		if result_dict["is_solvable"]:
 			break
 
-	# mr.visualize_maze(maze)
 	maze_population.put((maze_fitness, copy.deepcopy(maze)))
 
 print "initial population generated"
 
 for generation_count in range(generations):
 	for crossover_count in range(total_crossovers):
+
+		# keep generating child mazes until we find a solvable one
 		while True:
-			# choose two parent mazes at random
+			# choose two parent mazes at random from the population
 			parents = random.sample(range(0, population_size), 2)
-			# print parents
 
 			# combine the parents in some way to get the child maze
 			parent1_index = parents[0]
 			parent2_index = parents[1]
+
+			# choose a cross over point randomly
 			crossover_point = random.random()
 			crossover_column = int(crossover_point*dim)
 
+			# calculate contribution of both parents
 			parent1 = np.array(copy.deepcopy(maze_population.queue[parent1_index][1]), dtype=object)
 			parent1 = parent1[:, :crossover_column]
 			parent2 = np.array(copy.deepcopy(maze_population.queue[parent2_index][1]), dtype=object)
 			parent2 = parent2[:, crossover_column:]
 
+			# create child maze by combining the parents
 			child_maze = np.concatenate((parent1, parent2), axis=1)
 			child_maze = child_maze.tolist()
 
@@ -68,25 +85,21 @@ for generation_count in range(generations):
 			# print "Mutation Successful"
 
 			# compute the fitness of each child
-			manhattan_result, manhattan_steps, manhattan_max_fringe_length, \
-				manhattan_avg_fringe_length, manhattan_closed_set = \
+			child_result_dict = \
 				a_star_traversal(copy.deepcopy(child_maze), "manhattan")
 			
-			# claculate fitness using the max number of nodes explored
-			maze_fitness = -manhattan_steps
-			# print manhattan_result
-			if manhattan_result:
+			# calculate fitness using the max number of nodes explored
+			maze_fitness = -child_result_dict["total_steps"]
+		
+			if child_result_dict["is_solvable"]:
 				break
 		
-		# print manhattan_result, "checking child before adding it to population"
-		# mr.visualize_maze(child_maze)
 		maze_population.put((maze_fitness, copy.deepcopy(child_maze)))
 
 	# create new population using 90% best mazes and 10% worst mazes
 	best_mazes_count = int(0.9*population_size)
 	worst_mazes_count = population_size - best_mazes_count
 	fitness_average = 0
-	# pdb.set_trace()
 
 	new_maze_population = PriorityQueue()
 
@@ -106,22 +119,12 @@ for generation_count in range(generations):
 		(fitness, maze) = maze_population.get()
 		new_maze_population.put((fitness, copy.deepcopy(maze)))
 	
-	print "population solvability check"
-	for curr_maze_count in range(new_maze_population.qsize()):
-		curr_maze = new_maze_population.queue[curr_maze_count]
-		manhattan_result, manhattan_steps, manhattan_max_fringe_length, \
-			manhattan_avg_fringe_length, manhattan_closed_set = \
-			a_star_traversal(copy.deepcopy(curr_maze[1]), "manhattan")
-		if not manhattan_result:
-			print "ERROR!!!"
-			mr.visualize_maze(copy.deepcopy(curr_maze[1]))
-		print manhattan_result
-	print "--------------------"
-	
 	maze_population = new_maze_population
 
 			
 hardest_maze = maze_population.get()
 print "fitness of the hardest maze: ", hardest_maze[0]
 pdb.set_trace()
-mr.visualize_maze(hardest_maze[1])
+maze_runner.visualize_maze(hardest_maze[1])
+hardest_maze_result_dict = a_star_traversal(copy.deepcopy(hardest_maze[1]), "manhattan")
+maze_runner.visualize_explored_cells(hardest_maze[1], hardest_maze_result_dict["closed_set"])
