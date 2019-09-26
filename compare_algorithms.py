27bebe8a364f4769ef0eb72d7c146bd0bf25e597
Twisha Naik <twishanaik@Twishas-MacBook-Pro.local>
2019-09-25 22:23:58 -0400

    adding the function to compare different algorithms

--- compare_algorithms.py
+++ compare_algorithms.py
@@ -1,12 +1,15 @@
 import pandas as pd
 import copy 
-import maze_runner as mr
+import maze_runner
 from a_star import a_star_traversal
+from BFS import bfs
+from dfs import dfs_traversal
+from BiDirectionalBFS import bd_bfs
 
 # Main code
-dim = 20
-p = 0.2
-test_maze = mr.get_maze(dim, p)
+dim = 125
+p = 0.1
+test_maze = maze_runner.get_maze(dim, p)
 
 '''
 Given a maze, this function compares the different search algorithms based on:
@@ -19,20 +22,47 @@ def compare_algorithms(test_maze):
 	column_name_list = ['path_length', 'exploration_steps', 'max_fringe_size', 'avg_fringe_size']
 	algo_comparison_df = pd.DataFrame(columns=column_name_list)
 	
+	# BFS
+	maze = copy.deepcopy(test_maze)
+	result_dict = bfs(maze, len(maze))
+	path = maze_runner.get_path(dim-1, dim-1, maze)
+	df_entry = pd.DataFrame([(len(path), result_dict["total_steps"], result_dict["max_fringe_length"], 
+		result_dict["avg_fringe_length"])], columns = column_name_list, index=['BFS']) 
+	algo_comparison_df = algo_comparison_df.append(df_entry)
+
+	# DFS
+	maze = copy.deepcopy(test_maze)
+	result_dict = dfs_traversal(maze, len(maze))
+	path = maze_runner.get_path(dim-1, dim-1, maze)
+	df_entry = pd.DataFrame([(len(path), result_dict["total_steps"], result_dict["max_fringe_length"], 
+		result_dict["avg_fringe_length"])], columns = column_name_list, index=['DFS']) 
+	algo_comparison_df = algo_comparison_df.append(df_entry)
+
+	# BD-BFS
+	maze = copy.deepcopy(test_maze)
+	result_dict = bd_bfs(maze, len(maze))
+	path = maze_runner.get_path(dim-1, dim-1, maze)
+	df_entry = pd.DataFrame([(result_dict["path_length"], result_dict["total_steps"], result_dict["max_fringe_length"], 
+		result_dict["avg_fringe_length"])], columns = column_name_list, index=['BD-BFS']) 
+	algo_comparison_df = algo_comparison_df.append(df_entry)
+
 	# a-star with manhattan heuristic
 	maze = copy.deepcopy(test_maze)
-	result, exploration_steps, max_fringe_length, avg_fringe_length, closed_set = \
-		a_star_traversal(maze, "manhattan")
-	df_entry = pd.DataFrame([(exploration_steps, max_fringe_length, avg_fringe_length)], columns = column_name_list, index=['astar_manhattan']) 
+	result_dict = a_star_traversal(maze, "manhattan")
+	path = maze_runner.get_path(dim-1, dim-1, maze)
+	df_entry = pd.DataFrame([(len(path), result_dict["total_steps"], result_dict["max_fringe_length"], 
+		result_dict["avg_fringe_length"])], columns = column_name_list, index=['astar_manhattan']) 
 	algo_comparison_df = algo_comparison_df.append(df_entry)
 
 	# a-star with euclidian heuristic
 	maze = copy.deepcopy(test_maze)
-	result, exploration_steps, max_fringe_length, avg_fringe_length, closed_set = \
-		a_star_traversal(maze, "euclidian")
-	df_entry = pd.DataFrame([(exploration_steps, max_fringe_length, avg_fringe_length)], columns = column_name_list, index=['astar_euclidian']) 
-	algo_comparison_df.append(df_entry)
+	result_dict = a_star_traversal(maze, "euclidian")
+	path = maze_runner.get_path(dim-1, dim-1, maze)
+	df_entry = pd.DataFrame([(len(path), result_dict["total_steps"], result_dict["max_fringe_length"], 
+		result_dict["avg_fringe_length"])], columns = column_name_list, index=['astar_euclidian']) 
+	algo_comparison_df = algo_comparison_df.append(df_entry)
 
 	algo_comparison_df.to_csv("algo_comparison_df.csv")
 	return algo_comparison_df
 
+algo_comparison_df = compare_algorithms(test_maze)
