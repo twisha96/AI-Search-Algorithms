6107b864d75d60acd6a2959bc0d37d49a90f4108
haripriya2703 <haripriyaponnekanti@gmail.com>
2019-09-26 20:00:05 -0400

    static maze for dfs hardest maze

--- static_maze.py
+++ static_maze.py
@@ -1,34 +1,30 @@
 from cell import Cell
+import maze_runner as mr
+from dfs import dfs_traversal
+import copy
+maze = []
 
-def get_static_maze():
-    maze = []
+for row in xrange(15):
+    maze.append([])
+    for col in xrange(15):
+        maze[row].append(Cell(0))
 
-    for row in xrange(10):
-        maze.append([])
-        for col in xrange(10):
-            maze[row].append(Cell(0))
+maze[0][1].value = 1
+for col in range(2, 15, 1):
+    maze[5][col].value = 1
+maze[6][2].value = 1
+for col in range(2, 15, 1):
+    maze[11][col].value = 1
+maze[12][2].value = 1
 
-    for row in xrange(0, 10, 4):
-        maze[1][row].value = 1
-    for row in xrange(0, 10, 3):
-        maze[3][row].value = 1
-    for row in xrange(3, 10, 2):
-        maze[0][row].value = 1
-    for row in xrange(0, 10, 5):
-        maze[2][row].value = 1
-    for row in xrange(2, 10, 2):
-        maze[4][row].value = 1
-    for row in xrange(1, 10, 7):
-        maze[5][row].value = 1
-    for row in xrange(2, 10, 2):
-        maze[6][row].value = 1
-    for row in xrange(3, 10, 2):
-        maze[8][row].value = 1
-    maze[1][4].value = 0
-    maze[2][3].value = 1
-    maze[1][3].value = 1
-    maze[7][3].value = 1
+mr.visualize_maze(maze)
+result = dfs_traversal(maze, 15)
+path = mr.get_path(14, 14, copy.deepcopy(maze))
+print path
+mr.trace_path(copy.deepcopy(maze), path)
+fringe = result["fringe"]
+print result["max_fringe_length"]
+mr.visualize_explored_cells(copy.deepcopy(maze), fringe)
 
-    maze[0][0].value = 2
-    maze[9][9].value = 3
-    return maze
+
+
